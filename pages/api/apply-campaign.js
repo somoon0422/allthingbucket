f5e7ba47380c174338bdc7356bb0e06108dc11ed
unknown <19002957@thinkbig.co>
2025-09-10 15:27:59 +0900

    ES6 모듈을 CommonJS로 변경 - Vercel 호환성 개선

--- pages/api/apply-campaign.js
+++ pages/api/apply-campaign.js
@@ -1,6 +1,6 @@
-import clientPromise from '../../lib/mongodb';
+const clientPromise = require('../../lib/mongodb');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // CORS 헤더 설정
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST');
